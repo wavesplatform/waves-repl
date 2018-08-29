a7cc35c658fd403eef1a47b41007909eff5498e0
siem <siemarell@gmail.com>
2018-08-29 17:42:22 +0300

    main entry

--- src/repl/index.jsx
+++ src/repl/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './core/store';
-import registerServiceWorker from './registerServiceWorker';
 import './css/index.css';
 import './core/jsconsole.css';
 
