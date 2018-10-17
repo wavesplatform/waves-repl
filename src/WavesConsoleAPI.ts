4597684c0aa19281a590ab885f76120316b15517
siem <siemarell@gmail.com>
2018-10-17 20:19:22 +0300

    good errors on broadcast. corrent param merging

--- src/WavesConsoleAPI.ts
+++ src/WavesConsoleAPI.ts
@@ -15,7 +15,7 @@ export class WavesConsoleAPI {
     constructor() {
         Object.keys(wt).forEach(key => {
             this[key] = (params: any, seed: any) => (wt as any)[key](seed || WavesConsoleAPI.env.SEED,
-                {...params, chainId: WavesConsoleAPI.env.CHAIN_ID})
+                {chainId: WavesConsoleAPI.env.CHAIN_ID, ...params})
         })
     }
 
@@ -52,8 +52,14 @@ export class WavesConsoleAPI {
 
     public broadcast = async (tx: any, apiBase?: string) => {
         const url = new URL('transactions/broadcast', apiBase || WavesConsoleAPI.env.API_BASE).href;
-        const resp = await Axios.post(url, tx);
-        return resp.data
+        try{
+            const resp = await Axios.post(url, tx);
+            return resp.data
+        }catch (e) {
+            if (e.response && e.response.status === 400){
+                throw new Error(e.response.data.message)
+            }else throw e
+        }
     };
 
     public deploy = async (params?: { fee?: number, senderPublicKey?: string, script?: string }, seed?: string | string[]) => {
