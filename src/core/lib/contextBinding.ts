b4cee2635e7ccee72d34ba0132ebe59a597e4669
nbaymurzaev <nbaymurzaev@wv00352.local>
2019-02-18 18:42:24 +0300

    fix

--- src/core/lib/contextBinding.ts
+++ src/core/lib/contextBinding.ts
@@ -4,16 +4,17 @@ import { TTx, libs } from "@waves/waves-transactions/";
 import axios from 'axios';
 
 import {Console} from '../components/Console';
+import { byteToAddressOrAlias } from '@waves/marshall/dist/parsePrimitives';
 
 export const updateIFrameEnv = (env:any) => {
-    // add all waves functions to iframe global scope
     try {
-        WavesConsoleAPI.setEnv(env)
+        WavesConsoleAPI.setEnv(env);
+
         const iframeWindow = getContainer().contentWindow;
-        // Object.keys(consoleApi).forEach(key => iframeWindow[key] = w[key]);
-        iframeWindow['env'] = env
+
+        iframeWindow['env'] = env;
     } catch (e) {
-        console.error(e)
+        console.error(e);
     }
 };
 
@@ -38,21 +39,21 @@ interface IApiMethodWrappers {
     [key:string]: any
 };
 
-const getNetworkByte = (api: string) => {
-    return axios.get(`${api}/addresses`)
+const getNetworkByte = (apiBase: string) => {
+    return axios.get(`${apiBase}/addresses`)
         .then(res => {
             const address = res.data[0];
 
             const byte = libs.marshall.serializePrimitives.BASE58_STRING(address)[1];
             
             return String.fromCharCode(byte);
-        })
+        });
 };
 
 const getApiMethodWrappers = (consoleApi: WavesConsoleAPI, console: Console):  IApiMethodWrappers => {
     return {
-        broadcast: (tx: TTx, apiBase?: string) => {
-            const api = apiBase || WavesConsoleAPI.env.API_BASE;
+        broadcast: async (tx: TTx, apiBase: string = WavesConsoleAPI.env.API_BASE) => {
+            const nodes = ['https://nodes.wavesplatform.com', 'https://testnodes.wavesnodes.com'];
 
             const pushExplorerLinkToConsole = (href: string) => {
                 const htmlString = `<a href="${href}" target="_blank">Link to transaction in wavesexplorer</a>`;
@@ -64,41 +65,39 @@ const getApiMethodWrappers = (consoleApi: WavesConsoleAPI, console: Console):  I
                 });
             };
 
-            consoleApi.broadcast(tx, api)
-                .then((res: any) => {
-                    const wavesNodes = ['https://nodes.wavesplatform.com', 'https://testnodes.wavesplatform.com'];
+            const genereteExplorerLinkToTx = (networkByte: string, txId: number) => {
+                return (networkByte === 'W')
+                    ? `https://wavesexplorer.com/tx/${txId}`
+                    : `https://wavesexplorer.com/testnet/tx/${txId}`;
+            };
 
-                    if (wavesNodes.includes(api)) {
-                        const subdomain = api.split['.'][0];
+            try {
+                const res = await consoleApi.broadcast(tx, apiBase);
 
-                        const href = (subdomain === 'nodes')
-                            ? 'http://wavesexplorer.com/tx/${res.id}'
-                            : 'http://wavesexplorer.com/testnet/tx/${res.id}'
+                if (nodes.includes(apiBase)) {
+                    const networkByte = apiBase === 'https://nodes.wavesplatform.com' 
+                        ? 'W'
+                        : 'T';
+
+                    const href = genereteExplorerLinkToTx(networkByte, res.id);
+
+                    pushExplorerLinkToConsole(href);
+                } else {
+                    let networkByte = await getNetworkByte(apiBase);
+
+                    const isWavesNetwork = networkByte === 'W' || networkByte === 'T';
+
+                    if (isWavesNetwork) {
+                        const href = genereteExplorerLinkToTx(networkByte, res.id);
 
                         pushExplorerLinkToConsole(href);
-
-                        return res;
-
-                    } else {
-                        getNetworkByte(api)
-                            .then(networkByte => {
-
-                                const isWavesNetwork = networkByte === 'W' || networkByte === 'T';
-
-                                if (isWavesNetwork) {
-                                    const href = networkByte === 'W'
-                                        ? 'http://wavesexplorer.com/tx/${res.id}'
-                                        : 'http://wavesexplorer.com/testnet/tx/${res.id}'
-
-                                    pushExplorerLinkToConsole(href);
-
-                                    return res;
-                                };
-
-                                return res;
-                            });
                     };
-                })
+                };
+
+                return res;
+            } catch (error) {
+                return error;
+            };
         }
     };
 };
